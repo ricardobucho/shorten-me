a89acd3cf95eb52bbf2ec79654eb0de8b40f31e1
Ricardo Bucho <ricardobucho@live.com>
2021-02-01 20:13:35 +0000

    Add database configuration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190928185701) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "token", null: false
     t.text "url"
